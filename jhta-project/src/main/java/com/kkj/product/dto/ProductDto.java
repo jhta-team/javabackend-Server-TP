f94780ad5b774a74a6dfee58e4069c553bd20ca7
jarajiri <eic3021@gmail.com>
2023-10-18 17:32:18 +0900

    상품 리스트 페이지 css 구성

---
+++ jhta-project/src/main/java/com/kkj/product/dto/ProductDto.java
@@ -0,0 +1,97 @@
+package com.kkj.product.dto;
+
+public class ProductDto {
+	int id;
+	String pdtName;
+	String pdtCty;
+	String pdtContent;
+	int pdtPrice;
+	int pdtCount;
+	int pdtState;
+	String pdtDate;
+	String pdtModDate;
+	String pdtThum;
+	String pdtColor;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPdtName() {
+		return pdtName;
+	}
+	public void setPdtName(String pdtName) {
+		this.pdtName = pdtName;
+	}
+	public String getPdtCty() {
+		return pdtCty;
+	}
+	public void setPdtCty(String pdtCty) {
+		this.pdtCty = pdtCty;
+	}
+	public String getPdtContent() {
+		return pdtContent;
+	}
+	public void setPdtContent(String pdtContent) {
+		this.pdtContent = pdtContent;
+	}
+	public int getPdtPrice() {
+		return pdtPrice;
+	}
+	public void setPdtPrice(int pdtPrice) {
+		this.pdtPrice = pdtPrice;
+	}
+	public int getPdtCount() {
+		return pdtCount;
+	}
+	public void setPdtCount(int pdtCount) {
+		this.pdtCount = pdtCount;
+	}
+	public int getPdtState() {
+		return pdtState;
+	}
+	public void setPdtState(int pdtState) {
+		this.pdtState = pdtState;
+	}
+	public String getPdtDate() {
+		return pdtDate;
+	}
+	public void setPdtDate(String pdtDate) {
+		this.pdtDate = pdtDate;
+	}
+	public String getPdtModDate() {
+		return pdtModDate;
+	}
+	public void setPdtModDate(String pdtModDate) {
+		this.pdtModDate = pdtModDate;
+	}
+	public String getPdtThum() {
+		return pdtThum;
+	}
+	public void setPdtThum(String pdtThum) {
+		this.pdtThum = pdtThum;
+	}
+	public String getPdtColor() {
+		return pdtColor;
+	}
+	public void setPdtColor(String pdtColor) {
+		this.pdtColor = pdtColor;
+	}
+	
+	public ProductDto() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "ProductDto [id=" + id + ", pdtName=" + pdtName + ", pdtCty=" + pdtCty + ", pdtContent=" + pdtContent
+				+ ", pdtPrice=" + pdtPrice + ", pdtCount=" + pdtCount + ", pdtState=" + pdtState + ", pdtDate="
+				+ pdtDate + ", pdtModDate=" + pdtModDate + ", pdtThum=" + pdtThum + ", pdtColor=" + pdtColor + "]";
+	}
+	
+	
+	
+	
+	
+}
